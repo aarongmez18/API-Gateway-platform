b37a5a5161da1fa6286ad653ac5b72e25aa836b4
aarongmez18 <aarongmez18@gmail.com>
2026-08-17 17:06:56 +0200

    feat: created users service

---
+++ services/users-service/src/test/java/com/apigateway/users/UsersServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.apigateway.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
