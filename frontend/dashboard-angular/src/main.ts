0038ba0e9001247731a537d8b723e65dff1cdcfa
aarongmez18 <aarongmez18@gmail.com>
2026-08-31 17:33:28 +0200

    Connect roles with Angular Dashboard

--- frontend/dashboard-angular/src/main.ts
+++ frontend/dashboard-angular/src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { keycloak } from './app/core/auth/keycloak';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+async function start(): Promise<void> {
+  try {
+    await keycloak.init({ onLoad: 'login-required', pkceMethod: 'S256' });
+    console.log('Usuario autenticado:', keycloak.tokenParsed);
+    await bootstrapApplication(App, appConfig);
+  } catch (error) {
+    console.error('Error inicializando Keycloak:', error);
+  }
+}
+
+start();
