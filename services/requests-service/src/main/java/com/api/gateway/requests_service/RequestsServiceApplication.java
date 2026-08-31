e15d8d68ac2cbfa82b8e4c4370552e2bef831319
aarongmez18 <aarongmez18@gmail.com>
2026-08-31 21:25:57 +0200

    New request service

---
+++ services/requests-service/src/main/java/com/api/gateway/requests_service/RequestsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.api.gateway.requests_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestsServiceApplication.class, args);
+	}
+
+}
---
+++ services/requests-service/src/test/java/com/api/gateway/requests_service/RequestsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.gateway.requests_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
